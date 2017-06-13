0daf00b92442460e5b3abacd2bb3f7757fb741eb
shresthy <shresthjobs@gmail.com>
2017-06-14 03:06:22 +0530

    first com

---
+++ app.js
@@ -0,0 +1,19 @@
+var app=angular.module('myApp',[]);
+app.config(function(){
+
+Stamplay.init("shresth");
+
+
+})
+
+
+app.controller('myCtrl',function($scope){
+
+    
+   
+   $scope.ok="welcome to blog it!!!!";
+
+
+
+}
+)
